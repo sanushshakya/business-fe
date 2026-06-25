011f87a6f2a8b6e9d6bae4a9f96670020ee99622
sanushshakya <shakyasanush7@gmail.com>
2026-06-25 10:32:39 +0100

    feat(useInventory): add custom hook for fetching and managing inventory data

---
+++ src/hooks/useInventory.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from '../utils/axios';
+
+/**
+ * Custom hook to fetch inventory data.
+ *
+ * @returns {Object} - An object containing the loading state, error state, and inventory data.
+ */
+const useInventory = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    const fetchInventoryData = async () => {
+      try {
+        const response = await axios.get('/api/inventory/stock/levels/');
+        setInventory(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchInventoryData();
+  }, []);
+
+  return { loading, error, inventory };
+};
+
+export default useInventory;
