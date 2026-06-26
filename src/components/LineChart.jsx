e617f6db8ab279e1947cf412a2dd422b4a252128
sanushshakya <shakyasanush7@gmail.com>
2026-06-26 12:31:25 +0100

    feat: add LineChart component

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Line } from 'recharts';
+
+/**
+ * @module src/components/LineChart.jsx
+ *
+ * A component to render a line chart for displaying inventory projections.
+ */
+
+const data = [
+  // Example data points
+  { name: 'Day 1', value: 50 },
+  { name: 'Day 2', value: 60 },
+  { name: 'Day 3', value: 70 },
+  { name: 'Day 4', value: 80 },
+  { name: 'Day 5', value: 90 },
+  // Add more data points as needed
+];
+
+/**
+ * @function LineChart
+ *
+ * Renders a line chart with inventory projections.
+ *
+ * @param {Object} props - Component props
+ * @param {Array<Object>} props.data - Array of data points for the line chart
+ * @param {number} props.reorderThreshold - Reorder threshold to display as a dashed line
+ */
+const LineChart = ({ data, reorderThreshold }) => {
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      {reorderThreshold && (
+        <ReferenceLine
+          x={data.length - 1}
+          label={`Reorder at: ${reorderThreshold}`}
+          stroke="red"
+          strokeDasharray="5 5"
+        />
+      )}
+    </LineChart>
+  );
+};
+
+export default LineChart;
