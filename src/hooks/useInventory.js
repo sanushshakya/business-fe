74ce2dd8dc1a8aa327fac9bca9fc3e0c9018a93f
sanushshakya <shakyasanush7@gmail.com>
2026-06-27 10:43:42 +0100

    refactor(useInventory): update hook to include event details and demand alerts data

--- src/hooks/useInventory.js
+++ src/hooks/useInventory.js
@@ -2,20 +2,20 @@ import { useState, useEffect } from 'react';
 import axios from '../utils/axios';
 
 /**
- * Custom hook to fetch inventory data for the LineChart.
+ * Custom hook to fetch inventory data for the EventCard component.
  *
- * @returns {Object} - An object containing the loading state, error state, and inventory data.
+ * @returns {Object} - An object containing the loading state, error state, and event card data.
  */
 const useInventory = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [inventory, setInventory] = useState([]);
+  const [eventCards, setEventCards] = useState([]);
 
   useEffect(() => {
-    const fetchInventoryData = async () => {
+    const fetchEventCardData = async () => {
       try {
-        const response = await axios.get('/api/inventory/stock/levels/');
-        setInventory(response.data);
+        const response = await axios.get('/api/events/card/');
+        setEventCards(response.data);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -23,10 +23,10 @@ const useInventory = () => {
       }
     };
 
-    fetchInventoryData();
+    fetchEventCardData();
   }, []);
 
-  return { loading, error, inventory };
+  return { loading, error, eventCards };
 };
 
 export default useInventory;
