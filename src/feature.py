37270076bfde2bd7f0c6af30b3ab7075be7f9f57
sanushshakya <shakyasanush7@gmail.com>
2026-07-02 23:35:56 +0100

    feat: add new feature module

---
+++ src/feature.py
@@ -0,0 +1,42 @@
+# src/feature.py
+
+import React from 'react';
+import { Table, Modal, Button } from 'shadcn/ui';
+import useSyncManager from '../hooks/useSyncManager';
+
+/**
+ * MySuppliersPanel component to display the UserSupplier list in a table and manage suppliers.
+ * 
+ * @returns {React.FC} - The MySuppliersPanel component
+ */
+const MySuppliersPanel: React.FC = () => {
+  const { userSuppliers, openModal, closeModal } = useSyncManager();
+
+  return (
+    <div>
+      <Table data={userSuppliers}>
+        {/* Define the columns of the table */}
+        <Table.Column title="Name" accessor="name" />
+        <Table.Column title="Email" accessor="email" />
+        <Table.Column title="Phone" accessor="phone" />
+      </Table>
+
+      {/* Add Supplier form in a sheet/drawer */}
+      <Modal open={openModal} onClose={closeModal}>
+        <Modal.Header>Add New Supplier</Modal.Header>
+        <Modal.Body>
+          {/* Form fields for adding a new supplier */}
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="tel" placeholder="Phone" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary">Add Supplier</Button>
+          <Button variant="outline" onClick={closeModal}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default MySuppliersPanel;
