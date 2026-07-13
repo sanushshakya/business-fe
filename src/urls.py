8e15589ffdf95f9e6951189a486e71f2d1fcc82e
sanushshakya <shakyasanush7@gmail.com>
2026-07-13 19:11:39 +0100

    chore: add URL route for Invite User form submission

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AlertsDemandDetailView, AlertsDemandListView, StockReceiptView, OnboardingCompleteView, VerifyEmailView
+from .views import AlertsDemandDetailView, AlertsDemandListView, StockReceiptView, OnboardingCompleteView, VerifyEmailView, InviteUserFormView
 
 urlpatterns = [
     # Define URL patterns for different views
@@ -8,4 +8,5 @@ urlpatterns = [
     path('stock-receipts/', StockReceiptView.as_view(), name='stock-receipts'),
     path('api/tenants/company/onboarding-complete/', OnboardingCompleteView.as_view(), name='onboarding-complete'),  # New route for Waste Reduction Report
     path('api/auth/verify-email/', VerifyEmailView.as_view(), name='verify-email'),  # New route for email verification
+    path('invite-user/', InviteUserFormView.as_view(), name='invite-user-form'),  # New route to handle the Invite User form submission
 ]
